11175f35e575d02cd0ab8d70a188cc52661d68f3
nzshumate <nathanshumate0@gmail.com>
2018-10-16 20:43:02 -0500

    moving files to public repo

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+(function ($) {
+    'use strict';
+    jQuery(document).ready(function () {
+		// Counter
+		$('.counter').countTo();
+    //Hello World
+    console.log("Hello World, You really check your stuff huh? Fee free to give me a call if you have questions or want to colaborate - 501.316.8827");
+    });
+})(jQuery);
